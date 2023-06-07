f4ed6da3d419a5287e66a669ffa5384e9ea709de
Zoey Hu <54016472+zoeyzyhu@users.noreply.github.com>
2023-06-07 17:18:52 -0400

    README for pypi (#48)
    - Added README for `pypi` and `venv`
    - All input `toml` files for users should be placed in `src/pydisort`

--- setup.py
+++ setup.py
@@ -78,22 +78,26 @@ _here = os.path.abspath(os.path.dirname(__file__))
 version = {}
 with open(os.path.join(_here, 'src/pydisort', 'version.py'), encoding='utf-8') as f:
     exec(f.read(), version)
+with open(os.path.join(_here, 'doc/README_pypi.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 # Setup configuration
 setup(
     name='pydisort',
     version=version['__version__'],
     description='Modern Python interfece of DISORT.',
+    long_description=long_description,
     author='Zoey Hu',
     author_email='zoey.zyhu@gmail.com',
     license='GPL',
     packages=['pydisort'],
     package_dir={'': 'src'},
+    package_data={'pydisort': ['*.toml']},
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3 :: Only'
     ],
     python_requires=">=3.6",
     cmdclass={'build': extend_build()})
