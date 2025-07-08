3c5b426a55f9c9d0ad3580407267415b3e774c72
Tongsheng Wu <tongshengwu0@gmail.com>
2025-07-08 17:43:47 -0400

    test linking

---
+++ cmake/memorypool.cmake
@@ -0,0 +1,11 @@
+include(FetchContent)
+set(FETCHCONTENT_QUIET TRUE)
+
+set(PACKAGE_NAME memorypool)
+set(REPO_URL "https://github.com/tongshengw/memorypool")
+set(REPO_TAG "v0.1.10")
+
+add_package(${PACKAGE_NAME} ${REPO_URL} ${REPO_TAG} "" ON)
+set(FMT_INCLUDE_DIR
+  "${CMAKE_CURRENT_BINARY_DIR}/_deps/${PACKAGE_NAME}-src/include"
+  CACHE PATH "memorypool include directory")
